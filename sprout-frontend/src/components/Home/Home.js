a2a9268add03e3bf6ce99bd604ed44cafd3ebd4c
Matt Carroll <mwcarroll12@gmail.com>
2020-04-13 17:44:59 -0400

    added an add planter button. Spoiler, it adds plants

--- sprout-frontend/src/components/Home/Home.js
+++ sprout-frontend/src/components/Home/Home.js
@@ -11,14 +11,33 @@ class Home extends Component {
     this.state = {
       isLoading: true,
       token: '',
-      cards: [{name: "Planter A", type: "Lettuce", health: "high", nutrients: "high", harvest: "med"}],
+      cards: [{name: "Planter A", type: "Lettuce", health: "low", nutrients: "low", harvest: "med"},
+              {name: "Planter B", type: "Lettuce", health: "high", nutrients: "high", harvest: "med"}],
       name: 'My Account',
+      newId: '',
+      newType: '',
     };
 
+    this.addPlanterSubmit = this.addPlanterSubmit.bind(this);
+    this.planterIdChange = this.planterIdChange.bind(this);
+    this.planterTypeChange = this.planterTypeChange.bind(this);
   }
 
   logout() {
     alert("You logged out");
+  }
+
+  addPlanterSubmit() {
+    console.log('A planter was added: ' + this.state.newId + ' | type: ' + this.state.newType);
+    this.setState({cards: this.state.cards.concat([{name: this.state.newId, type: this.state.newType, health: "med", nutrients: "med", harvest: "med"}])});
+  }
+
+  planterIdChange(event) {
+    this.setState({newId: event.target.value});
+  }
+
+  planterTypeChange(event) {
+    this.setState({newType: event.target.value});
   }
 
   render() {
@@ -41,6 +60,36 @@ class Home extends Component {
           <h1 className="stage-title">
             Planters
           </h1>
+
+          <button style={{verticalAlign:'unset'}}type="button" class="btn btn-primary" data-toggle="modal" data-target="#addPlanterModal">Add Planter</button>
+          <div class="modal fade" id="addPlanterModal" tabindex="-1" role="dialog" aria-labelledby="addPlanterModal" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="addPlanterModal">Add New Planter</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form onSubmit={this.addPlanterSubmit}>
+                    <div class="form-group">
+                      <label for="planter-id" class="col-form-label">Planter Name:</label>
+                      <input type="text" value={this.state.newId} onChange={this.planterIdChange} class="form-control" id="planter-id"/>
+                    </div>
+                    <div class="form-group">
+                      <label for="message-text" class="col-form-label">Plant Type:</label>
+                      <input type="text" value={this.state.newType} onChange={this.planterTypeChange} class="form-control" id="planter-type"/>
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" onClick={this.addPlanterSubmit} class="btn btn-primary">Add</button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-5">
           {this.state.cards.map((card) => (
             <Link className="hidden-link" to={'planter/' + card.name}>
