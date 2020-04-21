adc35d27124d969cfc9b3f92ee2a13015ac037b4
Matt Carroll <mwcarroll12@gmail.com>
2020-04-21 04:16:06 -0400

    slight render issue if planters is undefined fixed

--- sprout-frontend/src/components/Farm/Farm.js
+++ sprout-frontend/src/components/Farm/Farm.js
@@ -336,7 +336,7 @@ class Farm extends Component {
               </div>
             </div>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-5">
-            {planters.length > 0 ? 
+            {planters && planters.length > 0 ? 
             planters.map((planter) => (
               <Link className="hidden-link" 
               to={{
